f2b9d414446faea183010a16aa95355c8145fd21
meetpatel2525 <meetjpatel521@gmail.com>
2023-02-24 22:53:48 +0530

    new project commit 2

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 var cors = require("cors");
 const user = require("./router/userRoute");
-// const url = "mongodb://localhost/demo";
+const url = "mongodb://localhost/demo";
 // mongodb://localhost:27017
-const url = process.env.MY_DB;
+// const url = process.env.MY_DB;
 require("dotenv").config();
 const path = require('path');
 const app = express();
