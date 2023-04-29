aaeb817e27d59a77ff3111513383178834766088
meetpatel2525 <meetjpatel521@gmail.com>
2023-04-29 12:07:20 +0530

    comment

--- controller/userController.js
+++ controller/userController.js
@@ -12,3 +12,19 @@ exports.insertUser = async(req, res) => {
         res.status(400).send(error);
     }
 };
+
+// const Comment = require('../models/comment');
+
+// router.get('/poststories', async (req, res) => {
+//   try {
+//     const postStories = await PostStory.find();
+//     const postStoriesWithComments = await Promise.all(postStories.map(async postStory => {
+//       const comments = await Comment.find({ postId: postStory._id }).sort({ createdAt: -1 }).limit(5);
+//       return { ...postStory.toJSON(), comments };
+//     }));
+//     res.json({ postStories: postStoriesWithComments });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// });
